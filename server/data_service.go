61760f2b0e4426a95c44bbd25e5c2307711370a5
oleg-rabaev <oleg@pubsubsql.com>
2013-11-11 18:08:28 -0400

    data_service add stubs

--- server/data_service.go
+++ server/data_service.go
@@ -17,17 +17,17 @@
 package pubsubsql
 
 type dataServiceItem struct {
-	req       *sqlRequest
+	req       request
 	responser *responseSender
 }
 
-// responseSender is responsible for channeling reponses to client connection 
+// dataService prer-processes sqlRequests and channels them to approptiate tables for further proccessging 
 type dataService struct {
 	requests chan dataServiceItem
 	stoper   *Stoper
 }
 
-// factory
+// dataService factory
 func newDataService(bufferSize int, stoper *Stoper) *dataService {
 	if stoper == nil {
 		panic("dataService.stoper can not be nil")
@@ -45,24 +45,42 @@ func (d *dataService) run() {
 	for {
 		select {
 		case item := <-d.requests:
-			onSqlRequest(item.req, item.responser)
+			d.onSqlRequest(item.req, item.responser)
 		case <-d.stoper.GetChan():
 			return
 		}
 	}
 }
 
-// 
-func onSqlRequest(r *sqlRequest, responser *responseSender) {
+func (d *dataService) onSqlRequest(r request, responser *responseSender) {
+	switch r.(type) {
+	case *sqlInsertRequest:
+		d.sqlInsert(r.(*sqlInsertRequest), responser)
 
-}
+	case *sqlSelectRequest:
+		d.sqlSelect(r.(*sqlSelectRequest), responser)
 
-func (d *dataService) findTable(table string) bool {
-	return false
-}
+	case *sqlUpdateRequest:
+		d.sqlUpdate(r.(*sqlUpdateRequest), responser)
 
-func (d *dataService) createTable(table string, columns []string) {
+	case *sqlDeleteRequest:
+		d.sqlDelete(r.(*sqlDeleteRequest), responser)
 
+	case *sqlSubscribeRequest:
+		d.sqlSubscribe(r.(*sqlSubscribeRequest), responser)
+
+	case *sqlUnsubscribeRequest:
+		d.sqlUnsubscribe(r.(*sqlUnsubscribeRequest), responser)
+
+	case *sqlKeyRequest:
+		d.sqlKey(r.(*sqlKeyRequest), responser)
+
+	case *sqlTagRequest:
+		d.sqlTag(r.(*sqlTagRequest), responser)
+
+	default:
+		panic("Unsuported sql request")
+	}
 }
 
 func (d *dataService) sqlInsert(req *sqlInsertRequest, responser *responseSender) {
@@ -70,5 +88,29 @@ func (d *dataService) sqlInsert(req *sqlInsertRequest, responser *responseSender
 }
 
 func (d *dataService) sqlSelect(req *sqlSelectRequest, responser *responseSender) {
+
+}
+
+func (d *dataService) sqlUpdate(req *sqlUpdateRequest, responser *responseSender) {
+
+}
+
+func (d *dataService) sqlDelete(req *sqlDeleteRequest, responser *responseSender) {
+
+}
+
+func (d *dataService) sqlSubscribe(req *sqlSubscribeRequest, responser *responseSender) {
+
+}
+
+func (d *dataService) sqlUnsubscribe(req *sqlUnsubscribeRequest, responser *responseSender) {
+
+}
+
+func (d *dataService) sqlKey(req *sqlKeyRequest, responser *responseSender) {
+
+}
+
+func (d *dataService) sqlTag(req *sqlTagRequest, responser *responseSender) {
 
 }
