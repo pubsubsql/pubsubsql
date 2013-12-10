d4b990d663814705d6eb4b68a80a45a37d75062d
oleg-rabaev <oleg@pubsubsql.com>
2013-12-10 11:48:15 -0400

    network,response add unversioned files

---
+++ server/network_test.go
@@ -0,0 +1,27 @@
+/* Copyright (C) 2013 CompleteDB LLC.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with PubSubSQL.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package pubsubsql
+
+import "testing"
+
+func TestNetworkStartStop(t *testing.T) {
+	n := newNetwork()
+	if !n.start("localhost:54321") {
+		t.Error(`network.start("localhost:54321") failed`)
+	}
+	n.stop()
+}
