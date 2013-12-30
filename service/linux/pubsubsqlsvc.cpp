b5b28510ff9a9b84ec0e36d54791ed0f51291b83
oleg-rabaev <oleg@pubsubsql.com>
2013-12-30 19:13:13 -0400

    linux service add readable error handling

--- service/linux/pubsubsqlsvc.cpp
+++ service/linux/pubsubsqlsvc.cpp
@@ -9,8 +9,23 @@
 
 int install(const char* serviceFile, const std::string& options);
 int uninstall();
-int runAsDaemon(char* path, char** argv);
+void rundaemon(char* path, char** argv);
 void* logthread(void *);
+
+// wrap "must succeede" system calls into class to avoid error handling spagetti
+class os {
+public:
+	static void pipe(int fildes[2], const char* desc);
+	static int fork(const char* desc);
+	static void close(int fd, const char* desc);
+	static void dup2(int oldfd, int newfd, const char* desc);
+	static void execvp(const char *file, char *const argv[], const char* desc);
+	static void pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine) (void *), void *arg, const char* desc);
+	static void waitpid(pid_t pid, int *status, int options, const char* desc);
+	static FILE* fdopen(int fd, const char *mode, const char* desc);
+private:
+	static void logfatal(const char* err, const char* desc);
+};
 
 int logpipe[2];
 const char* logprefix = "pubsubsql";
@@ -34,7 +49,7 @@ int main(int argc, char** argv) {
 			std::cerr << "expected executable file path" << std::endl;
 			return EXIT_FAILURE;
 		}
-		return runAsDaemon(argv[2], argv + 2);
+		rundaemon(argv[2], argv + 2);
 	}
 	// invalid command
 	std::cerr << "invalid command: " << command << usage << std::endl;
@@ -51,47 +66,35 @@ int uninstall() {
 	return EXIT_SUCCESS;
 }
 
-int runAsDaemon(char* path, char** argv) {
-	// create pipe to redirect stderr to syslog
-	pipe(logpipe);
+void rundaemon(char* path, char** argv) {
+	os::pipe(logpipe, "create pipe to redirect stderr to syslog");
 	int status = 0;
-	pid_t childPid = fork();
-	//
+	pid_t childPid = os::fork("forking child process");
 	switch (childPid) {
-	case -1:
-		break;
 	case 0: // start pubsubsql
-		// close read end of the logpipe
-		close(logpipe[0]);
+		os::close(logpipe[0], "close read end of the logpipe");
 		// associate pipe with stderr
 		if (logpipe[1] != STDERR_FILENO) {
-			dup2(logpipe[1], STDERR_FILENO);
-			close(logpipe[1]);
+			os::dup2(logpipe[1], STDERR_FILENO, "duplicate write pipe->STDERR_FILENO");
+			os::close(logpipe[1], "close duplicated write pipe->STDERR_FILENO");
 		}
-		execvp(path, argv);
+		os::execvp(path, argv, "starting pubsubsql");
 		break;
 	default:
-		// close write end of the logpipe
-		close(logpipe[1]);
-		// redirect err from pubsubsql to syslog			
-		pthread_t tx;
-		pthread_create(&tx, NULL, logthread, NULL);
-		if (waitpid(childPid, &status, 0) == -1) {
-			break;
-		} 
-		pthread_join(tx, NULL);
-		break;
+		os::close(logpipe[1], "close write end of the logpipe");
+		// start thread to redirect err from pubsubsql to syslog			
+		pthread_t thread;
+		os::pthread_create(&thread, NULL, logthread, NULL, "start logger thread");
+		os::waitpid(childPid, &status, 0, "waiting for child pubsubsql");
+		pthread_join(thread, NULL);
+		_exit(status);
 	}		
-
-	return EXIT_SUCCESS;
+	_exit(EXIT_SUCCESS);
 }
 
 void* logthread(void *) {
+	FILE* f = os::fdopen(logpipe[0], "r", "set stream in logthread");	
 	openlog(logprefix, LOG_PERROR, LOG_USER);
-	FILE* f = fdopen(logpipe[0], "r");	
-	if (NULL == f) {
-		return NULL;
-	} 
 	const int BUFFER_SIZE = 4096;	
 	char buffer[1 + BUFFER_SIZE] = {0};
 	for (;;) {
@@ -113,5 +116,51 @@ void* logthread(void *) {
 	}
 	closelog();
 	return NULL;
+}
+
+// os
+void os::pipe(int fildes[2], const char* desc) {
+	if (-1 == ::pipe(fildes)) logfatal("pipe() failed", desc);
+}
+
+int os::fork(const char* desc) {
+	int ret = ::fork();
+	if (-1 == ret) logfatal("fork() failed", desc);
+	return ret;
+}
+
+void os::close(int fd, const char* desc) {
+	if (-1 == ::close(fd)) logfatal("close() failed", desc);		
+}
+
+void os::dup2(int oldfd, int newfd, const char* desc) {
+	if (-1 == ::dup2(oldfd, newfd)) logfatal("dup2() failed", desc);	
+}
+
+void os::execvp(const char *file, char *const argv[], const char* desc) {
+	::execvp(file, argv);
+	logfatal("execvp() failed", desc);	
+}
+
+void os::pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine) (void *), void *arg, const char* desc) {
+	if (0 != ::pthread_create(thread, attr, start_routine, arg)) logfatal("pthread_create() failed", desc);
+}
+
+void os::waitpid(pid_t pid, int *status, int options, const char* desc) {
+	if (-1 == ::waitpid(pid, status, options)) logfatal("waitpid() failed", desc);
+}
+
+FILE* os::fdopen(int fd, const char *mode, const char* desc) {
+	FILE* ret = ::fdopen(fd, mode);
+	if (NULL == ret) logfatal("fdopen() failed", desc);
+	return ret;
+}
+
+// since we are not running in a terminal session, log to syslog and exit the process
+void os::logfatal(const char* err, const char* desc) {
+	openlog(logprefix, LOG_PID | LOG_PERROR, LOG_USER);
+	syslog(LOG_EMERG, "%s %s", err, desc);
+	closelog();
+	_exit(EXIT_FAILURE);
 }
 
