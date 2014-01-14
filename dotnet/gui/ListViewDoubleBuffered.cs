4011507313a5b86e503154f7a37c25acf5acf01d
oleg-rabaev <oleg@pubsubsql.com>
2014-01-14 11:39:46 -0500

    .Net gui add missing files

---
+++ dotnet/gui/ListViewDoubleBuffered.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PubSubSQLGUI
+{
+    class ListViewDoubleBuffered : System.Windows.Forms.ListView
+    {
+        public ListViewDoubleBuffered()
+            : base()
+        {
+            DoubleBuffered = true;
+        }
+    }
+}
