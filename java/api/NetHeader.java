5e3738a8630794e7062f946be92f474236a3bfbd
oleg-rabaev <oleg@pubsubsql.com>
2014-01-22 14:51:26 -0400

    Java Client add client interface

--- java/api/NetHeader.java
+++ java/api/NetHeader.java
@@ -1,4 +1,3 @@
-
 /* Copyright (C) 2014 CompleteDB LLC.
  *
  * This program is free software: you can redistribute it and/or modify
--- java/build.bat
+++ java/build.bat
@@ -1,3 +1,3 @@
-javac -d . api/NetHeader.java
-jar cfv pubsubsql.jar pubsubsql/NetHeader.class
+javac -d . api/NetHeader.java api/Client.java
+jar cfv pubsubsql.jar pubsubsql/NetHeader.class pubsubsql/Client.class
 javac -d . -cp pubsubsql.jar test/PubSubSQLTest.java
