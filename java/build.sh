30f182548f34559b8dabecb3b89db95ca549195f
oleg-rabaev <oleg@pubsubsql.com>
2014-02-07 09:26:59 -0500

    java move to separate repository

--- java/build.sh
+++
@@ -1,4 +0,0 @@
-javac -d . -cp .:lib/gson-2.2.4.jar api/NetHeader.java api/Client.java api/ClientImpl.java api/Factory.java api/NetHelper.java api/ResponseData.java 
-jar cfv lib/pubsubsql.jar pubsubsql/NetHeader.class pubsubsql/Client.class pubsubsql/ClientImpl.class pubsubsql/Factory.class pubsubsql/NetHelper.class pubsubsql/ResponseData.class
-javac -d . -cp .:lib/* test/PubSubSQLTest.java
-java -cp .:lib/* PubSubSQLTest
--- java/buildgui.sh
+++
@@ -1,8 +0,0 @@
-#javac -d . gui/PubSubSQLGUI.java gui/MainForm.java
-#jar cvmf gui/main_manifest pubsubsqlgui.jar PubSubSQLGUI.class PubSubSQLGUI1$.class MainForm.class gui/images/New.png
-#java -jar pubsubsqlgui.jar
- 
-javac -d . gui/PubSubSQLGUI.java gui/MainForm.java
-jar cvf pubsubsqlgui.jar PubSubSQLGUI.class MainForm.class gui/images/New.png
-jar cfe pubsubsqlgui.jar PubSubSQLGUI PubSubSQLGUI.class
-java -jar pubsubsqlgui.jar
